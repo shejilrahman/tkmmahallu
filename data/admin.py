4699aff1e14af148c19d2f95113e748ed148ec93
Sirajudheen Rahiman <vn04337@hebcom>
2022-12-22 22:43:19 +0530

    Made age as options for persons

--- data/admin.py
+++ data/admin.py
@@ -7,20 +7,20 @@ from .models import Family,Member,Ward
 
 
 
-# @admin.action(description='Mark selected person as Family head')
-# def make_head(modeladmin, request, queryset):
-#     queryset.update(status='H')
-#     raw_id_fields = ('family',)
+@admin.action(description='Mark selected person as Family head')
+def make_head(modeladmin, request, queryset):
+    queryset.update(status='H')
+    raw_id_fields = ('family',)
 
 
-# class ArticleAdmin(admin.ModelAdmin):
-#     list_display = ['name', 'status','job','age','blood_group','family']
-#     list_filter=['family']
-#     search_fields=['name','family','blood_group']
-#     ordering = ['family']
-#     actions = [make_head]
-#     search_fields = ('name_of_family_head',)
-#     raw_id_fields = ('family',)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'status','job','age','blood_group','family']
+    list_filter=['family']
+    search_fields=['name','family','blood_group']
+    ordering = ['family']
+    actions = [make_head]
+    search_fields = ('name_of_family_head',)
+    raw_id_fields = ('family',)
 
 
 admin.site.register(Family)
