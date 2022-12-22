ef4c60bbcc1b13e342a824a345c202fcd340c17f
Sirajudheen Rahiman <vn04337@hebcom>
2022-12-22 19:34:42 +0530

    d

--- data/management/commands/createsu.py
+++ data/management/commands/createsu.py
@@ -7,4 +7,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin")
+            User.objects.create_superuser("admin", "admin@admin.com", "admindfasfasfasf")
