02976687841314c27ee62932611ba65f50388656
Sirajudheen Rahiman <vn04337@hebcom>
2022-12-22 19:00:31 +0530

    d

--- palli/settings.py
+++ palli/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-1p*@@xxs-f9^v__5(8tm!v2ce20n+fv0u*_+qf(0%^r!z7wgwv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tkmmahallu-dev.ap-southeast-1.elasticbeanstalk.com', '127.0.0.1']
+ALLOWED_HOSTS = ['tkmmahallu-dev.ap-southeast-1.elasticbeanstalk.com', '127.0.0.1', '172.31.15.160']
 
 
 # Application definition
